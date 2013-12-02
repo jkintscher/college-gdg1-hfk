bc2047bb0f956efb0cddc1b5993e66a98a452c28
Joschka <joschka@kintscher.me>
2013-12-02 17:44:16 +0100

    Implement Floor class to better distribute responsibilities

--- assets/js/engine.js
+++ assets/js/engine.js
@@ -5,21 +5,41 @@ $(function() {
       width  = canvas.width(),
       height = canvas.height(),
       floors = [],
+      GLOBAL_OFFSET = 0,
       FLOOR_COUNT = 10,
       TILE_COUNT = 20,
       TILE_WIDTH = width / TILE_COUNT,
       FLOOR_HEIGHT = height / FLOOR_COUNT;
 
-  var generateFloor = function() {
-    var floor = [],
-        holes = 0;
-    for(var i = 0; i < TILE_COUNT; ++i) {
-      floor[i] = !(holes < 2 && Math.random() > 0.8);
-      if(!floor[i]) {
-        holes++;
+  var Floor = function() {
+    var tiles = [];
+
+    var generateFloor = function() {
+      var holes = 0;
+      for(var i = 0; i < TILE_COUNT; ++i) {
+        tiles[i] = !(holes < 2 && Math.random() > 0.8);
+        if(!tiles[i]) {
+          holes++;
+        }
       }
+    };
+
+    generateFloor();
+
+    this.render = function(offset) {
+      var tile_offset  = 0;
+      ctx.beginPath();
+      for(var i = 0; i < tiles.length; ++i) {
+        ctx.moveTo(tile_offset, offset);
+        tile_offset += TILE_WIDTH;
+        if(tiles[i]) {
+          ctx.lineTo(tile_offset, offset);
+        }
+      }
+      ctx.stroke();
     }
-    return floor;
+
+    return this;
   };
 
   var render = function() {
@@ -29,33 +49,22 @@ $(function() {
     ctx.lineWidth = 6;
 
     for(var i = 0; i < floors.length; ++i) {
-      renderFloor(i, floors[i]);
+      floors[i].render(GLOBAL_OFFSET + FLOOR_HEIGHT * (i + 1));
     }
   };
 
-  var renderFloor = function(floor, tiles) {
-    var floor_offset = FLOOR_HEIGHT * floor,
-        tile_offset  = 0;
-    ctx.beginPath();
-
-    for(var i = 0; i < tiles.length; ++i) {
-      ctx.moveTo(tile_offset, floor_offset);
-      tile_offset += TILE_WIDTH;
-
-      if(tiles[i]) {
-        ctx.lineTo(tile_offset, floor_offset);
-      }
-
-    }
-    ctx.stroke();
-  }
-
   var init = function() {
     for(var i = 0; i < FLOOR_COUNT; ++i) {
-      floors.push(generateFloor());
+      floors.push(new Floor());
     }
 
-    setInterval(render, 24);
+    setInterval(function() {
+      GLOBAL_OFFSET--;
+      if((GLOBAL_OFFSET % FLOOR_HEIGHT) == 0) {
+        floors.push(new Floor());
+      }
+      render();
+    }, 24);
   };
 
   init();
