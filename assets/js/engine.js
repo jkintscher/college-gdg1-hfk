8a27e287b2b8feb7a3e554a6cd90de24674f0084
Joschka <joschka@kintscher.me>
2014-02-03 01:38:20 +0100

    Create images prior to executing game engine.

--- assets/js/engine.js
+++ assets/js/engine.js
@@ -1,3 +1,30 @@
+var IMAGES_PATH = './assets/images/',
+    get_image = function(path) {
+      var img = new Image();
+      img.src = IMAGES_PATH + path + '.png';
+      return img;
+    },
+    IMAGES = {
+      backgrounds: [
+        get_image('background-1'),
+        get_image('background-2')
+      ],
+      stairs: [
+        get_image('stairs-top'),
+        get_image('stairs-bottom')
+      ],
+      character: {
+        left:  [
+          get_image('character-left-1'),
+          get_image('character-left-2')
+        ],
+        right: [
+          get_image('character-right-1'),
+          get_image('character-right-2')
+        ]
+      }
+    };
+
 $(function() {
   var canvas = $('#canvas'),
       ctx    = canvas[0].getContext('2d'),
